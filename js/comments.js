a64bf637189e32a217f0f57f901b1633509e1067
David Nelson Murray <linearsix@gmail.com>
2018-08-19 12:50:47 -0500

    basic main card functionality and styling working

---
+++ js/comments.js
@@ -0,0 +1,35 @@
+    // COIN MARKET CAP API
+    // endpoint overview
+    // https://pro.coinmarketcap.com/api/v1/cryptocurrency/info?CMC_PRO_API_KEY=00ca39a2-10a7-4617-8eef-897fa860bd8b
+    // include in header
+    // X-CMC_PRO_API_KEY: 00ca39a2-10a7-4617-8eef-897fa860bd8b
+    // CMC_PRO_API_KEY=00ca39a2-10a7-4617-8eef-897fa860bd8b
+    // Example API call
+    // https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
+    // https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&start=0&limit=10&cryptocurrency_type=tokens&convert=USD,BTC
+
+    // CRYPTOCURRENCYCHART.COM
+    // https://www.cryptocurrencychart.com/api/coin/list
+    // Email	linearsix@gmail.com
+    // API key	8d2c3de8569dcba4698b8aa3a07b5110
+    // API secret	aedf01c65f35ce81219512c5c333c8a8
+    // https://www.cryptocurrencychart.com/api/documentation
+
+    // COINAPI.IO
+    // https://docs.coinapi.io/#introduction
+    // X-CoinAPI-Key: 345FCF08-5D2C-432A-8FA1-7B4972E7FD53
+    // FETCH URL: https://rest.coinapi.io/v1/exchanges?apikey=345FCF08-5D2C-432A-8FA1-7B4972E7FD53
+
+    // data_end:"2018-08-18"
+    // data_orderbook_end:"2018-08-18T15:27:54.8920000Z"
+    // data_orderbook_start:"2017-12-18T21:50:58.3910192Z"
+    // data_quote_end:"2018-08-18T15:27:54.8920000Z"
+    // data_quote_start:"2017-12-18T21:50:58.3910192Z"
+    // data_start:"2017-07-14"
+    // data_symbols_count:394
+    // data_trade_count:460919764
+    // data_trade_end:"2018-08-18T15:27:39.8240000Z"
+    // data_trade_start:"2017-07-14T04:00:00.3760000Z"
+    // exchange_id:"BINANCE"
+    // name:"Binance"
+    // website:"https://www.binance.com/"
--- js/index.js
+++ js/index.js
@@ -1,46 +1,7 @@
 console.log(`coin`);
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    // ATTRIBUTES TO LOOK FOR
-    // market cap
-    // coins in circulation
-    // year of origin
-    // anything about the team
-    // anything about the consensus method like “proof of work“
-    // price
-    // % price gain or price loss over given time
-    // anything about philosophy or purpose of coin would be fantastic- basically what is the value prop.
-
-    // COIN MARKET CAP API
-    // endpoint overview
-    // https://pro.coinmarketcap.com/api/v1#section/Endpoint-Overview
-    // include in header
-    // X-CMC_PRO_API_KEY: 00ca39a2-10a7-4617-8eef-897fa860bd8b
-    // Example API call
-    // https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
-    // https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap&start=0&limit=10&cryptocurrency_type=tokens&convert=USD,BTC
-
-    // COINAPI.IO
-    // https://docs.coinapi.io/#introduction
-    // X-CoinAPI-Key: 345FCF08-5D2C-432A-8FA1-7B4972E7FD53
-    // Plan name: Free
-    // https://rest.coinapi.io/
-
-    // data_end:"2018-08-18"
-    // data_orderbook_end:"2018-08-18T15:27:54.8920000Z"
-    // data_orderbook_start:"2017-12-18T21:50:58.3910192Z"
-    // data_quote_end:"2018-08-18T15:27:54.8920000Z"
-    // data_quote_start:"2017-12-18T21:50:58.3910192Z"
-    // data_start:"2017-07-14"
-    // data_symbols_count:394
-    // data_trade_count:460919764
-    // data_trade_end:"2018-08-18T15:27:39.8240000Z"
-    // data_trade_start:"2017-07-14T04:00:00.3760000Z"
-    // exchange_id:"BINANCE"
-    // name:"Binance"
-    // website:"https://www.binance.com/"
-
+    
     // TEST OBJECT FOR OFFLINE TESTING
     const testData = [
         {"name": "BitCoin", "exchange_id": "000001"},
@@ -65,15 +26,50 @@ document.addEventListener('DOMContentLoaded', function() {
         {"name": "Hubby Pro", "exchange_id": "000020"},
     ]
 
-    // I DON'T THINK THIS IS ACTUALLY DOING ANYTHING
-    let headerCoinApi = new Headers({
-        'X-CoinAPI-Key': '345FCF08-5D2C-432A-8FA1-7B4972E7FD53',
-        'Accept': 'application/json',
-        'Accept-Encoding': 'deflate, gzip'
-    });
+    // ATTRIBUTES TO LOOK FOR
+    // market cap
+    // coins in circulation
+    // year of origin
+    // anything about the team
+    // anything about the consensus method like “proof of work“
+    // price
+    // % price gain or price loss over given time
+    // anything about philosophy or purpose of coin would be fantastic- basically what is the value prop.
+
+    // COINAPI.IO
+    // https://docs.coinapi.io/#introduction
+    // X-CoinAPI-Key: 345FCF08-5D2C-432A-8FA1-7B4972E7FD53
+    // FETCH URL: https://rest.coinapi.io/v1/exchanges?apikey=345FCF08-5D2C-432A-8FA1-7B4972E7FD53
+
+    // data_end:"2018-08-18"
+    // data_orderbook_end:"2018-08-18T15:27:54.8920000Z"
+    // data_orderbook_start:"2017-12-18T21:50:58.3910192Z"
+    // data_quote_end:"2018-08-18T15:27:54.8920000Z"
+    // data_quote_start:"2017-12-18T21:50:58.3910192Z"
+    // data_start:"2017-07-14"
+    // data_symbols_count:394
+    // data_trade_count:460919764
+    // data_trade_end:"2018-08-18T15:27:39.8240000Z"
+    // data_trade_start:"2017-07-14T04:00:00.3760000Z"
+    // exchange_id:"BINANCE"
+    // name:"Binance"
+    // website:"https://www.binance.com/"
+
+    let urlParams = new URLSearchParams(window.location.search);
+    let searchData = (urlParams.get(`searchSelect`));
+    // console.log(searchData);
 
     let selectExchange = document.getElementById(`selectMain`);
     // console.log(selectExchange);
+
+    let divMiddleLeft = document.getElementById(`middleLeft`);
+    let cardMain = document.createElement(`div`);
+        cardMain.className = (`cardMain`);
+        
+    let divMainGuts = document.createElement(`div`);
+        divMainGuts.className = (`divMainGuts`);
+    // Populate window with coin listing.
+    // guide user to click thumbnail, add input or select from dropdown list
     
     // COMMENT OUT NEXT FOUR LINES FOR OFFLINE TESTING
     fetch("https://cors-anywhere.herokuapp.com/https://rest.coinapi.io/v1/exchanges?apikey=345FCF08-5D2C-432A-8FA1-7B4972E7FD53")
@@ -82,11 +78,10 @@ document.addEventListener('DOMContentLoaded', function() {
         // console.log(data);
         let coinObjs = data;//testData; 
         let divScroll = document.getElementById(`middleRight`);
-        let divThumbColor;
 
         for (let coinObj in coinObjs) {
             // POPULATE SELECT OPTIONS
-            console.log(coinObjs[coinObj]);
+            // console.log(coinObjs[coinObj]);
             let exchangeName = coinObjs[coinObj].name;
             let exchangeId = coinObjs[coinObj].exchange_id;
             // console.log(exchangeName)
@@ -95,12 +90,28 @@ document.addEventListener('DOMContentLoaded', function() {
             selectOption.value = (`${exchangeId}`);
             selectExchange.appendChild(selectOption);
 
-            // POPULATE THUMBNAILS
-            // let startArr = coinObjs[coinObj].data_trade_start;
-            // startArr.split(`T`, 17);
-            // let start = startArr[0];
-            // console.log(start);
+            // POPULATE MAIN CARD
+            if (searchData) {
+                if (searchData === exchangeId) {
+                    console.log(searchData);
+                    console.log(exchangeId);
+                    
+                    cardMain.style.background = (`#${intToRGB(hashCode(exchangeName))}`);
+                    cardMain.textContent = (`${exchangeName}`);
+                    divMiddleLeft.appendChild(cardMain);
 
+                    divMainGuts.innerHTML = (`
+                    Origin: ${coinObjs[coinObj].data_start.split("T")[1]}</p>
+                    <p>Start:</br>${coinObjs[coinObj].data_trade_start}</p>
+                    <p>End:</br>${coinObjs[coinObj].data_trade_end}</p>
+                    <p>ID:${coinObjs[coinObj].exchange_id}</p>
+                    <p>${coinObjs[coinObj].website}</p>
+                    `);
+                    cardMain.appendChild(divMainGuts);
+                };
+            };
+
+            // POPULATE THUMBNAILS
             let divThumb = document.createElement(`div`);
             divThumb.className = (`divThumb`);
             divThumb.textContent = (`${exchangeName}`);
